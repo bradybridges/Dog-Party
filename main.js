dbe0a0c76f21046af301007879f17780ea8ec1c1
Brady Bridges <bradyjbridges@gmail.com>
2019-06-29 23:53:11 -0600

    Add Change Button Margin On Click

--- main.js
+++ main.js
@@ -6,30 +6,39 @@ function addName(){
 
 function toggleHow(){
 	var how = document.getElementById("how");
+	var howButton = document.getElementById("how-button");
 
 	if(how.style.display === "none"){
 		how.style.display = "block";
+		howButton.style.marginTop = "2em";
 	}else{
 		how.style.display = "none";
+		howButton.style.marginTop = "5em";
 	}
 }
 
 function toggleWhat(){
 	var what = document.getElementById("what");
+	var whatButton = document.getElementById("what-button");
 
 	if(what.style.display === "none"){
 		what.style.display = "block";
+		whatButton.style.marginTop = "2em";
 	}else{
 		what.style.display = "none";
+		whatButton.style.marginTop = "5em";
 	}
 }
 
 function toggleFacts(){
 	var facts = document.getElementById("facts");
+	var factsButton = document.getElementById("facts-button");
 
 	if(facts.style.display === "none"){
 		facts.style.display = "block";
+		factsButton.style.marginTop = "2em";
 	}else{
 		facts.style.display = "none";
+		factsButton.style.marginTop = "5em";
 	}
 }
